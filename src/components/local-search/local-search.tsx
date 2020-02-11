750a06798485d743c7a9687be477f9fddd2b4bfe
James Hill <jahill@groupon.com>
2020-02-11 13:39:12 +0000

    Added linting configs and packages

--- src/components/local-search/local-search.tsx
+++ src/components/local-search/local-search.tsx
@@ -1,55 +1,57 @@
 import { Component, Prop, h, getAssetPath, State } from '@stencil/core';
 
 @Component({
-  tag: 'local-search',
-  styleUrl: 'local-search.scss',
-  assetsDirs: ['assets'],
-  shadow: true
+   tag: 'local-search',
+   styleUrl: 'local-search.scss',
+   assetsDirs: ['assets'],
+   shadow: true,
 })
 export class LocalSearch {
-  /**
-   * Main title text
-   */
-  @Prop() titleText: string;
+   /**
+    * Main title text
+    */
+   @Prop() titleText: string;
 
-  /**
-   * Map link text
-   */
-  @Prop() linkText: string;
+   /**
+    * Map link text
+    */
+   @Prop() linkText: string;
 
-  /**
-   * Map URL
-   */
-  @Prop() mapUrl: string;
+   /**
+    * Map URL
+    */
+   @Prop() mapUrl: string;
 
-  /**
-   * Internal input state
-   */
-  @State() searchValue: string;
+   /**
+    * Internal input state
+    */
+   @State() searchValue: string;
 
-  private onKeyUp = ({ target }) => {
-    this.searchValue = target.value;
-  }
+   private onKeyUp = ({ target }) => {
+      this.searchValue = target.value;
+   };
 
-  private onSubmit = (ev) => {
-    ev.preventDefault();
+   private onSubmit = ev => {
+      ev.preventDefault();
 
-    console.log('submit', this.searchValue);
-  }
+      console.log('submit', this.searchValue);
+   };
 
-  render() {
-    return (
-      <div class="wrapper">
-        <div class="content">
-          <h3 class="title">{this.titleText}</h3>
-          <form class="form" method="POST" onSubmit={this.onSubmit}>
-            <input class="input" type="text" onKeyUp={this.onKeyUp} />
-            <button class="button ">Show Offers</button>
-          </form>
-          <a href={this.mapUrl} class="link" target="_blank">{this.linkText}</a>
-        </div>
-        <img class="image" src={getAssetPath('./assets/map.png')} />
-      </div>
-    );
-  }
+   render() {
+      return (
+         <div class="wrapper">
+            <div class="content">
+               <h3 class="title">{this.titleText}</h3>
+               <form class="form" method="POST" onSubmit={this.onSubmit}>
+                  <input class="input" type="text" onKeyUp={this.onKeyUp} />
+                  <button class="button ">Show Offers</button>
+               </form>
+               <a href={this.mapUrl} class="link" target="_blank">
+                  {this.linkText}
+               </a>
+            </div>
+            <img class="image" src={getAssetPath('./assets/map.png')} />
+         </div>
+      );
+   }
 }
--- stencil.config.ts
+++ stencil.config.ts
@@ -2,22 +2,20 @@ import { Config } from '@stencil/core';
 import { sass } from '@stencil/sass';
 
 export const config: Config = {
-  plugins: [
-    sass()
-  ],
-  namespace: 'local-map',
-  outputTargets: [
-    {
-      type: 'dist',
-      esmLoaderPath: '../loader'
-    },
-    {
-      type: 'docs-readme'
-    },
-    {
-      type: 'www',
-      serviceWorker: null, // disable service workers
-      baseUrl: 'https://gb.v3.idldev.net',
-    }
-  ]
+   namespace: 'local-map',
+   plugins: [sass()],
+   outputTargets: [
+      {
+         type: 'dist',
+         esmLoaderPath: '../loader',
+      },
+      {
+         type: 'docs-readme',
+      },
+      {
+         type: 'www',
+         serviceWorker: null, // disable service workers
+         baseUrl: 'https://gb.v3.idldev.net',
+      },
+   ],
 };
